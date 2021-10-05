90d9125a6defe6f2c332655c87db6ab196098a84
mayshareves <mayshareves@gmail.com>
2021-10-05 22:49:52 +0300

    css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
-import Header from '../Header/Header'
-import Products from '../Products/Products'
+import Header from './Header/Header'
+import Products from './Products/Products'
 function Main() {
     return (
     <div>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,7 +1,11 @@
 import Product from "../Product/Product";
+import './Products.css';
 function Products() {
     return (
         <div className="products">
+            <Product></Product>
+            <Product></Product>
+            <Product></Product>
             <Product></Product>
             <Product></Product>
             <Product></Product>
